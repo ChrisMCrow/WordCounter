3dd38f6569b03045a971f906a37f912178010808
Chris Crow <chrismcrow@gmail.com>
2018-09-14 16:52:55 -0700

    fix bug where the final word of the sentence was not being checked.

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -43,7 +43,7 @@ namespace WordCounter.Models
       string[] words = StringToArray();
       _word.ToLower();
 
-      for(int i = 0; i < words.Length - 1; i ++)
+      for(int i = 0; i < words.Length; i ++)
       {
         _matches += (_word == words[i]) ? 1 : 0;
       }
